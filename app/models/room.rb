ac1677be875f39af63dc5730e076424c8369c742
skinkm <140519195+skinkm@users.noreply.github.com>
2023-08-19 22:57:30 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :user_rooms
-  
-  validates :name, presence: true
+  has_many :messages
 
+  validates :name, presence: true
 end
